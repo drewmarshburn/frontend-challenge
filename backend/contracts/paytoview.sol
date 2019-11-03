06e91dc03e7bd1a0600b21c1236f8b869638c3f9
rdmarshb <rdmarshb@ncsu.edu>
2019-11-03 08:22:06 -0500

    Fixed paytoview

--- backend/contracts/paytoview.sol
+++ backend/contracts/paytoview.sol
@@ -1,6 +1,8 @@
 pragma solidity >=0.4.24 <0.6.0;
 
-//import "./coininterface.sol";
+contract CoinInterface {
+    function transfer(address _to, uint256 _value) public returns (bool success);
+}
 
 /**
   * @title paytoview
@@ -9,9 +11,8 @@ pragma solidity >=0.4.24 <0.6.0;
 contract paytoview {
     address public owner;
     uint public price;
-    //CoinInterface public ffContract;
+    CoinInterface public ffContract;
     string private secret;
-    address public ffContract;
 
     /* Stores addresses of those allowed to view the secret */
     mapping (address => bool) public allowedToView;
@@ -19,32 +20,23 @@ contract paytoview {
     /**
       * @dev Sets the owner, price, secret, and exchange contract
       */
-    constructor(address _owner, string memory _secret, uint _price, address _ffCtrAddr) public {
-        owner = _owner;
+    constructor(string memory _secret, uint _price, address _ffCtrAddr) public {
+        owner = msg.sender;
         secret = _secret;
         price = _price;
-        //ffContract = CoinInterface(_ffCtrAddr);
-        ffContract = _ffCtrAddr;
+        ffContract = CoinInterface(_ffCtrAddr);
         allowedToView[owner] = true;
-    }
-
-    /**
-      * @dev Set the secret value. (Probably should be removed!)
-      */
-    function setSecret(string memory _secret) public {
-        require(msg.sender == owner);
-        secret = _secret;
     }
 
     /**
      * @dev Buy the secret
      */
-     function buySecret(address _purchaser) public {
+     function buySecret() public {
          require(msg.sender != owner, "You are the ownder.");
          require(!allowedToView[msg.sender], "You own this already.");
-         //require(ffContract.transfer(owner, price));
+         require(ffContract.transfer(owner, price));
 
-         allowedToView[_purchaser] = true;
+         allowedToView[msg.sender] = true;
      }
 
     /**
